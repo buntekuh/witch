34f93577248caa02c522efc1dc04a661e241b31b
buntekuh <me@u.de>
2021-07-04 12:27:42 +0200

    simplifying build command

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
-sjasmplus --lst $1.asm
+#!/bin/sh
+if [[ $# -ne 1 ]]
+then
+    file=witch
+else
+    file=$1
+fi
+
+sjasmplus --lst $file.asm
 if [ $? -eq 0 ]; then
-    fuse $1.sna
+    fuse $file.sna
 fi
